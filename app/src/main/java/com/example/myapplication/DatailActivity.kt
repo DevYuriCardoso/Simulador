e824d0303e4c0bb520ef3d809359ff9705ab6959
DevYuriCardoso <yuricardoso.dev@proton.me>
2023-08-08 23:13:47 -0300

    Desenvolvimento dos Layouts  de Listagens e detalhes das partidas

---
+++ app/src/main/java/com/example/myapplication/DatailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityDatailBinding
+
+class DatailActivity : AppCompatActivity() {
+    private var binging: ActivityDatailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binging = ActivityDatailBinding.inflate(layoutInflater)
+
+
+        setContentView(binging.root)
+        setSupportActionBar(binging.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
